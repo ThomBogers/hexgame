4eb2793dd102a5e4ea4bebd1ee3d02a46f11e836
Thom <thom.bogers@gmail.com>
2017-02-11 17:03:53 +0100

    initiall comit, generate grid

---
+++ Assets/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+
+    public Color color;
+
+    // Use this for initialization
+    void Start () {
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(0f, 1f);
+        float b = Random.Range(0f, 1f);
+        float a = 0f;
+
+        color = new Color(r, g, b, a);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+
+    }
+}
---
+++ Assets/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public int height;
+    public int width;
+    
+    public float buffer;
+
+    public Cell cellPrefab;
+
+    Cell[] cells;
+
+    // Use this for initialization
+	void Start () {
+        print("Setting up grid");
+        cells = new Cell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++) {
+            for (int x = 0; x < width; x++) {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int index) {
+        print("Creating cell");
+        Vector3 position; 
+        position.x = x * 10f + buffer;
+        position.y = 0f + buffer;
+        position.z = z * 10f +buffer;
+
+        Cell cell = cells[index] = Instantiate<Cell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
